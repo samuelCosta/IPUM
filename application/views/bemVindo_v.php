ad614ad5436f6362f59bf7926fe8ecab4825d189
JoanaMatos <fifas1622@gmail.com>
2016-03-14 21:05:22 +0000

    Teste Joana

--- application/views/bemVindo_v.php
+++ application/views/bemVindo_v.php
@@ -72,7 +72,9 @@
             <!-- Left side column. contains the logo and sidebar -->
             <aside class="main-sidebar">
                 <!-- sidebar: style can be found in sidebar.less -->
-                <section class="sidebar">
+                <section class="sidebar" >
+                    
+                </section>
                     <!-- Sidebar user panel -->
                     <div class="user-panel">
                         <div class="pull-left image">
