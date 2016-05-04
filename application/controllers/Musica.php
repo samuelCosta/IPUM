9933ccfdaaf0622d73ae0a3edcf42eb5e0a981aa
pgonc92 <paulo.goncalves.92@gmail.com>
2016-05-04 19:52:04 +0100

    commmit_2

--- application/controllers/Musica.php
+++ application/controllers/Musica.php
@@ -4,103 +4,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Musica extends CI_Controller {
 
+    function __construct() {
+        parent::__construct();
+        $this->load->model('instrumento_m');
+        $this->load->model('tiposelecao_m');
+    }
+
     public function index() {
+//        if ($this->session->userdata('logged_in')) {
+        $data['musicas'] = $this->musica_m->get_musicas();
 
         $this->load->view('includes/header_v');
-        $this->load->view('registarMusica_v');
+        $this->load->view('musica/index', $data);
         $this->load->view('includes/menu_v');
-        $this->load->view('includes/footer_v');
+//        }  else {
+//            redirect('Welcome', 'refresh');
+//        }
     }
-
-    public function criarMusica() {
-
-        $this->load->view('includes/header_v');
-        $this->load->view('registarMusica_v');
-        $this->load->view('includes/menu_v');
-        $this->load->view('includes/footer_v');
-    }
-
-    public function registarMusica() {
-
-        $this->form_validation->set_rules('nomeMusica', 'Nome', 'required');
-        $this->form_validation->set_rules('video', 'Vídeo', 'required');
-
-        if ($this->form_validation->run() == FALSE) {
-            $this->load->view('includes/header_v');
-            $this->load->view('registarMusica_v');
-            $this->load->view('includes/menu_v');
-            $this->load->view('includes/footer_v');
-        } else {
-            //insere os dados na base de dados
-            $dados = elements(array('nomeMusica', 'video'), $this->input->post());
-            $this->load->model('Musica_m');
-            $this->Musica_m->do_insert($dados);
-
-            $data['msg'] = "Sucesso.";
-            $this->load->view('includes/header_v');
-            $this->load->view('includes/msgSucesso_v', $data);
-            $this->load->view('registarMusica_v');
-            $this->load->view('includes/menu_v');
-            $this->load->view('includes/footer_v');
-        }
-    }
-
-    public function consultarMusica() {
-        $this->load->model('Musica_m');
-        $dados['musica'] = $this->Musica_m->get_Musica();
-
-        $this->load->view('includes/header_v');
-        $this->load->view('consultarMusica_v', $dados);
-        $this->load->view('includes/menu_v');
-        $this->load->view('includes/footer_v');
-    }
-
-    public function atualizar($id = null, $indice = null) {
-
-        $this->load->model('Musica_m');
-        $data['musica'] = $this->Musica_m->compararId($id);
-
-
-        $this->load->view('includes/header_v');
-        $this->load->view('editarMusica_v', $data);
-        $this->load->view('includes/menu_v');
-        $this->load->view('includes/footer_v');
-    }
-
-    public function guardarAtualizacao() {
-        //strtolower-colocar tudo em minusculo
-        //ucwords-colocar iniciais em maiusculo
-        
-        $this->form_validation->set_rules('nomeMusica', 'Nome', 'required');
-        $this->form_validation->set_rules('video', 'Vídeo', 'required');
-        $id = $this->input->post('idMusica');
-
-
-        if ($this->form_validation->run() == FALSE) {
-
-            $this->load->model('Musica_m');
-            $data['musica'] = $this->Musica_m->compararId($id);
-
-            $this->load->view('includes/header_v');
-            $this->load->view('editarMusica_v', $data);
-            $this->load->view('includes/menu_v');
-            $this->load->view('includes/footer_v');
-        } else {
-            //insere os dados na base de dados
-            $dados = elements(array('nomeMusica', 'video'), $this->input->post());
-            
-            $this->load->model('Musica_m');
-            $this->Musica_m->guardarAtualizacao($id, $dados);
-
-            $data['msg'] = "Alterado com Sucesso.";
-            $this->load->model('Musica_m');
-            $dados['musica'] = $this->Musica_m->get_Musica();
-            $this->load->view('includes/header_v');
-            $this->load->view('includes/msgSucesso_v', $data);
-            $this->load->view('consultarMusica_v', $dados);
-            $this->load->view('includes/menu_v');
-            $this->load->view('includes/footer_v');
-        }
-    }
-
 }
--- application/models/Musica_m.php
+++ application/models/Musica_m.php
@@ -4,28 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Musica_m extends CI_Model {
 
-    public function do_insert($dados = NULL) {
-        if ($dados != NULL):
-            $this->db->insert('musica', $dados);
-        endif;
-    }
     
-    public function get_Musica() {
-
-        $this->db->select('*');
-        return $this->db->get('musica')->result();
-    }
-    
-    public function compararId($id) {
-
-        $this->db->where('idMusica', $id);
-        return $this->db->get('musica')->result();
-    }
-    
-    public function guardarAtualizacao($id, $data) {
-
-        $this->db->where('idMusica', $id);
-        return $this->db->update('musica', $data);
-    }
 
 }
